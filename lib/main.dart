30c5d1c7b5ef38407c59a1216619b2911d8d46e8
Andrei Campigotto <andreicampigotto@gmail.com>
2022-10-03 19:33:31 -0300

    adjusted main

--- lib/main.dart
+++ lib/main.dart
@@ -43,7 +43,7 @@ class MyApp extends StatelessWidget {
           AppRoutes.HOME: (context) => const ProductsOverviewPage(),
           AppRoutes.PRODUCT_DETAIL: (context) => const ProductDetailPage(),
           AppRoutes.CART: (context) => const CartPage(),
-          AppRoutes.ORDERS: (context) => const OrdersPage(),
+          AppRoutes.ORDERS: (context) => OrdersPage(),
           AppRoutes.PRODUCTS: (context) => const ProductsPage(),
           AppRoutes.PRODUCT_FORM: (context) => const ProductFormPage(),
         },
